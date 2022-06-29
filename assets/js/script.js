06a2bd0db64e3769c831b5d34706676fc65c9032
zeyno12 <zeynabbagirova1@gmail.com>
2022-06-30 02:34:12 +0400

    zeynab

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+function zeynuw(){
+    const url = "http://127.0.0.1:5500/second.html"
+    window.open(url);
+}
